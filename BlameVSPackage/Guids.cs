e525dcabe3ec30f6f262d1a3b6bbf6a1aca97fce
Gerard Walace <gerard.walace@gmail.com>
2015-06-13 06:33:40 +0200

    Creation du projet de Package (utilisation du template VisualStudio)

---
+++ BlameVSPackage/Guids.cs
@@ -0,0 +1,16 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace GerardWalace.BlameVSPackage
+{
+    static class GuidList
+    {
+        public const string guidBlameVSPackagePkgString = "479bb051-92f3-49d3-9e85-040c928d6b80";
+        public const string guidBlameVSPackageCmdSetString = "17f0af00-f6aa-49dc-aa07-c1a30adf66dd";
+        public const string guidBlameVSPackageEditorFactoryString = "01bb0228-1e41-4a4e-b185-e02a74682d13";
+
+        public static readonly Guid guidBlameVSPackageCmdSet = new Guid(guidBlameVSPackageCmdSetString);
+        public static readonly Guid guidBlameVSPackageEditorFactory = new Guid(guidBlameVSPackageEditorFactoryString);
+    };
+}
---
+++ BlameVSPackage/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BlameVSPackage")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("GerardWalace")]
+[assembly: AssemblyProduct("BlameVSPackage")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]   
+[assembly: ComVisible(false)]     
+[assembly: CLSCompliant(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+[assembly: InternalsVisibleTo("BlameVSPackage_IntegrationTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001000f1a3995549ded61a6967a1f3c8b39426b2850f4f4a22be02c1de0cdeba5850001ed75138fa95d88e416dae7bf70170486954cda1a650025e36f6092cfb2d56bcb70e2fbd230fa810dbec55a5e0bd82aa62a093e9b3ebd06ec3f162f72187a45c5cf7379c0c9f24702d2160034d4b85bdb5ac217c20b79f1836163f81c7dda9e")]
+[assembly: InternalsVisibleTo("BlameVSPackage_UnitTests, PublicKey=00240000048000009400000006020000002400005253413100040000010001000f1a3995549ded61a6967a1f3c8b39426b2850f4f4a22be02c1de0cdeba5850001ed75138fa95d88e416dae7bf70170486954cda1a650025e36f6092cfb2d56bcb70e2fbd230fa810dbec55a5e0bd82aa62a093e9b3ebd06ec3f162f72187a45c5cf7379c0c9f24702d2160034d4b85bdb5ac217c20b79f1836163f81c7dda9e")]
